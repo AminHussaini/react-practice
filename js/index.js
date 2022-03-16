efdfe9aa698e564b0279cc9b82521daeb88f3ac1
AminHussaini <59252100+AminHussaini@users.noreply.github.com>
2022-03-16 22:48:49 +0500

    First Static website

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// Youtube Video
+// ReactDOM.render(<h1>Hello, everyone!</h1>,document.getElementById('root'));
+
+// Test 
+// ReactDOM.render(<h1>Hello world</h1>,document.getElementById("root"))
+
+// Test 2 
+ReactDOM.render(<ul><li>Apple</li><li>Orange</li><li>Banana</li></ul>,document.getElementById("root"))
